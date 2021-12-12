0b33c3ad08c1a046592a64bdff8d13f87650cff9
hbjs97 <hbjs97@naver.com>
2021-12-13 02:54:25 +0900

    fix, auth router

--- festival-map-web/src/components/auth/PublicRoute.js
+++ festival-map-web/src/components/auth/PublicRoute.js
@@ -5,8 +5,7 @@ import { LOGIN_SUCCESS } from '../../redux/modules/login';
 
 function PublicRoute({ component: Component, restricted, ...rest }) {
   const loginStates = useSelector((state) => state.login);
-  // const isLogin = loginStates.type === LOGIN_SUCCESS;
-  const isLogin = true;
+  const isLogin = loginStates.type === LOGIN_SUCCESS;
   return <Route {...rest} render={(props) => (!isLogin && restricted ? <Redirect to="/login" /> : <Component {...props} />)} />;
 }
 
