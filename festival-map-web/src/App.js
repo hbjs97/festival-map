cb89ad78d10946c99d6d3614a65da257611ed3b5
hbjs97 <hbjs97@naver.com>
2021-12-13 07:34:11 +0900

    feat, boards routes

--- festival-map-web/src/App.js
+++ festival-map-web/src/App.js
@@ -7,6 +7,7 @@ import Login from './components/Login';
 import ParkingLot from './components/ParkingLot';
 import Board from './components/Board';
 import BoardView from './components/BoardView';
+import BoardEnroll from './components/BoardEnroll';
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
         <PublicRoute restricted={false} component={SignUp} path="/signup" exact />
         <PublicRoute restricted={false} component={Login} path="/login" exact />
         <PublicRoute restricted={false} component={ParkingLot} path="/parking-lot" exact />
-        <PrivateRoute component={Board} path="/board" exact />
+        <PublicRoute restricted={false} component={Board} path="/board" exact />
+        <PrivateRoute component={BoardEnroll} path="/board/enroll" exact />
         <PrivateRoute component={BoardView} path="/board/:id" exact />
         <PublicRoute restricted={false} component={Blog} path="/" exact />
       </Switch>
