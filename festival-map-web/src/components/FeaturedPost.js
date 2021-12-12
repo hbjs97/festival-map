3ed27b4ad78184d5fafb7bf7c46280e137c957c8
hbjs97 <hbjs97@naver.com>
2021-12-13 02:56:15 +0900

    fix, featured post

--- festival-map-web/src/components/FeaturedPost.js
+++ festival-map-web/src/components/FeaturedPost.js
@@ -12,37 +12,36 @@ function FeaturedPost(props) {
 
   return (
     <Grid item xs={12} md={6}>
-      <CardActionArea component="a" href="#">
+      <CardActionArea component="a" href={post.homepageUrl ? (post.homepageUrl.includes('http') ? post.homepageUrl : `http://${post.homepageUrl}`) : undefined}>
         <Card sx={{ display: 'flex' }}>
           <CardContent sx={{ flex: 1 }}>
             <Typography component="h2" variant="h5">
-              {post.title}
+              {post.fstvlNm}
             </Typography>
-            <Typography variant="subtitle1" color="text.secondary">
-              {post.date}
-            </Typography>
+            {post.fstvlStartDate && (
+              <Typography variant="subtitle1" color="text.secondary">
+                {`${post.fstvlStartDate} ~ ${post.fstvlEndDate}`}
+              </Typography>
+            )}
+            {post.opar && (
+              <Typography variant="subtitle1" paragraph>
+                {post.opar}
+              </Typography>
+            )}
+            {post.phoneNumber && (
+              <Typography variant="subtitle1" paragraph>
+                {post.phoneNumber}
+              </Typography>
+            )}
             <Typography variant="subtitle1" paragraph>
-              {post.description}
-            </Typography>
-            <Typography variant="subtitle1" color="primary">
-              Detail
+              {post.rdnmadr || post.lnmadr}
             </Typography>
           </CardContent>
-          {post.image && <CardMedia component="img" sx={{ width: 160, display: { xs: 'none', sm: 'block' } }} image={post.image} alt={post.imageLabel} />}
+          {<CardMedia component="img" sx={{ width: 160, display: { xs: 'none', sm: 'block' } }} image={'https://source.unsplash.com/random'} />}
         </Card>
       </CardActionArea>
     </Grid>
   );
 }
-
-FeaturedPost.propTypes = {
-  post: PropTypes.shape({
-    date: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    imageLabel: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-  }).isRequired,
-};
 
 export default FeaturedPost;
