175896708ad4c8fb9a347635fda50c607ed7a2cb
hbjs97 <hbjs97@naver.com>
2021-12-13 02:54:00 +0900

    fix, zoom option

--- festival-map-web/src/lib/config.js
+++ festival-map-web/src/lib/config.js
@@ -2,7 +2,9 @@ import { defaultPosition } from './constant';
 
 export const mapOptions = {
   center: new naver.maps.LatLng(defaultPosition),
-  zoom: 15,
+  zoom: 13,
+  minZoom: 10,
+  maxZoom: 17,
   zoomControl: true,
   zoomControlOptions: {
     style: naver.maps.ZoomControlStyle,
